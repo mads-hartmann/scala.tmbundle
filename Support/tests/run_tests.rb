7869bad184dca08798d6b17772a641c4fd2c2858
Mads Hartmann <mads379@gmail.com>
2010-08-16 00:36:08 +0200

    Fixed a bug where import.scala.NodeSeq was wrongly highlighted. NodeSeq wasn't inferred as variable.import.scala - it is now. Added a test cases for the import statements

--- Support/tests/run_tests.rb
+++ Support/tests/run_tests.rb
@@ -3,13 +3,13 @@ module RunTests
   module_function
   
   def compare(name)
-    result = `./gtm < input/#{name}.scala ../Syntaxes/Scala.tmLanguage`.split("\n")
+    result = `./gtm < input/#{name}.scala ../../Syntaxes/Scala.tmLanguage`.split("\n")
     expected = `cat output/#{name}.output`.split("\n")
 
     booleans = Array.new(result.count) { |index|
       result[index] == expected[index]
     }
-    booleans.uniq.count == 1
+    booleans.uniq.count == 1 && booleans.uniq[0] == true
   end
   
 end
@@ -19,3 +19,9 @@ puts "Simple class: " + RunTests.compare("class").to_s
 
 ## This tests a simple method definition
 puts "Simple method: " + RunTests.compare("method").to_s
+
+## Import with brackets, ie. scala.xml.NodeSeq
+puts "Simple import: " + RunTests.compare("import").to_s
+
+## Import without brackets, ie. scala.xml.NodeSeq
+puts "Simple import2: " + RunTests.compare("import2").to_s
