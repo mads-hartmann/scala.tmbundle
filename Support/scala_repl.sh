db85912dc4d7889be905046867a81ccbe92401b1
Mads Hartmann <mads379@gmail.com>
2010-11-05 00:58:17 +0100

    Attempt to fix applescript bug seen on inkytonik's computer

--- Support/scala_repl.sh
+++ Support/scala_repl.sh
@@ -6,11 +6,10 @@ tell application "Terminal"
 	if (count of windows) is 0 then
 		do script "$CMD"
 	else
-		set window_id to id of first window whose frontmost is true
     tell application "System Events"
       keystroke "t" using {command down}
     end
-    do script "$CMD" in window id window_id of application "Terminal"
+    do script "$CMD" in window 1
 	end if
 end tell
 END
